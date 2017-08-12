45748c9d4b00cc4328e8274d4c70825778ff6c1a
Tim McKee <timothy.mckee@example.com>
2017-08-11 22:52:40 -0400

    redux action repos

--- src/actions/RepoActions.js
+++ src/actions/RepoActions.js
@@ -12,17 +12,31 @@ const url = `${GITHUB_REPOS}`;
 
 export function getUserRepos() {
   console.log(`github url: ${url}`);
-  const request = axios.get(`${GITHUB_REPOS}`);
+  return dispatch => {
+    dispatch(fetchRepos());
+    fetch(url)
+      .then(data => data.json())
+      .then(json => {
+        console.log("json:", json);
+        dispatch(fetchReposSuccess(json.results));
+      })
+      .catch(err => dispatch(fetchReposFailure(err)));
+  };
+}
+
+export function fetchRepos() {
+  console.log("fetchRepos");
   return {
-    type: FETCH_REPOS,
-    payload: request
+    type: FETCH_REPOS
   };
 }
 
 export function fetchReposSuccess(repos) {
+  console.log("fetchReposSuccess", repos);
   return { type: FETCH_SUCCESS, payload: repos };
 }
 
 export function fetchReposFailure(error) {
+  console.log("fetchReposFailure", error);
   return { type: FETCH_FAILURE, payload: error };
 }
